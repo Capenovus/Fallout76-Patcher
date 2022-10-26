3c0bf33ea50b8bcd6cc241d8b9e510da98b28feb
Capenovus <Magnusgorus2@gmail.com>
2022-10-26 14:31:54 +0200

    minor changes

--- Fallout76-Patcher/fcrc32.bat
+++ Fallout76-Patcher/fcrc32.bat
@@ -3,11 +3,5 @@
 :: stops writing commands in script
 @echo off
 
-:: title of the console
-title muy retrasado patch script
-
-:: press something to continue
-pause
-
 :: executes fcrc32.py
 python fcrc32.py SeventySix.esm 12 799A1939
